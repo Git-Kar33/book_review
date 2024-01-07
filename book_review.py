f21b2a0503bb8d02145bb8c1a721c1f6a745ba27
Ekram <djekram33@gmail.com>
2024-01-07 12:43:56 +0300

    Final code

---
+++ book_review.py
@@ -0,0 +1,29 @@
+import os
+from pyairtable import Api
+
+class BookReview:
+    def __init__(self):
+        self.api = Api(os.environ['AIRTABLE_API'])
+        self.table = self.api.table('appC6GNe2lurweMDU', 'tblVG149zf1kQYLPN')
+
+    def get_book_ratings(self, sort="ASC", max_records=10):
+        if not sort:
+            return self.table.all(max_records=max_records)
+        elif sort == "ASC":
+            rating = ["Rating"]
+        elif sort == "DESC":
+            rating = ["-Rating"]
+        
+        table = self.table.all(sort=rating, max_records=max_records)
+        return table
+
+    def add_book_rating(self, book_title, book_rating, notes=None):
+        fields = {'Book': book_title, 'Rating': book_rating, 'Notes': notes}
+        self.table.create(fields=fields)
+
+
+if __name__ == '__main__':
+    br = BookReview()
+    # br.add_book_rating('Infinite Jest', 7.0)
+    get_book_ratings = br.get_book_ratings(sort="DESC", max_records=1)
+    print(get_book_ratings)
